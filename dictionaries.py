1820b2b659b7d4411cc2ba238cfbb348e2f331de
Dhanushka <dhanushkamcr@gmail.com>
2025-08-12 18:45:24 -0500

    Adding, modifying and removing elements from dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -18,3 +18,26 @@ print(my_dict['name'])  # Output: Alice
 # The .get() method & avoiding missing keys 
 print(my_dict.get('name'))  # Output: Alice
 print(my_dict.get('ads'))  # Output: None
+
+
+
+# # Adding, Modifying, and Removing Elements
+# Adding Elements:
+print(my_dict)
+my_dict['profession'] = 'Engineer'
+print(my_dict)
+
+# Modifying Elements:
+my_dict['profession'] = 'Changed profession'
+print(my_dict)
+
+# Removing Elements:
+del my_dict['profession']
+print(my_dict)
+
+removed_value = my_dict.pop(1)
+print(removed_value)  # Output: 777777
+print(my_dict)
+removed_value = my_dict.pop(1, "There is no key with ypour input")
+print(removed_value)  # Output: 777777
+print(my_dict)
