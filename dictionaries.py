fffd3c5ad44b1a04672cb59e7f0b7e7d0c36c95f
Dhanushka <dhanushkamcr@gmail.com>
2025-08-12 18:54:57 -0500

    Dictionary methods

--- dictionaries.py
+++ dictionaries.py
@@ -41,3 +41,23 @@ print(my_dict)
 removed_value = my_dict.pop(1, "There is no key with ypour input")
 print(removed_value)  # Output: 777777
 print(my_dict)
+
+
+
+# # Dictionary Methods
+# .keys():
+print(my_dict.keys())  # Output: dict_keys(['name', 'age', 'city'])
+print(list(my_dict.keys()))  # Output: ['name', 'age', 'city']
+for key in my_dict.keys():
+    print(key)
+    
+# .values():
+print(my_dict.values())  # Output: dict_values(['Alice', 25, 'New York'])
+print(list(my_dict.values()))  # Output: ['Alice', 25, 'New York']
+for value in my_dict.values():
+    print(value)
+
+# .items():
+print(list(my_dict.items()))  # Output: [('name', 'Alice'), ('age', 25), ('city', 'New York')]
+for key, value in my_dict.items():
+    print(f"Key \"{key}\" holds \"{value}\" value")
