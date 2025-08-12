3bfe439c624c27cf7fecf2e45c35c7d36bdcb240
Dhanushka <dhanushkamcr@gmail.com>
2025-08-12 18:23:51 -0500

    Tuple methods and concatination

--- tuples.py
+++ tuples.py
@@ -90,3 +90,19 @@ def display_info(name, age, profession):
 info_tuple = ("Charlie", 28, "Designer")
 
 display_info(*info_tuple) 
+
+
+
+# # Tuple Methods
+# .count():
+my_tuple = (1, 2, 2, 3, 2)
+print(my_tuple.count(2))  # Output: 3
+print(len(my_tuple))  # Output: 3
+
+# .index():
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple.index(3))  # Output: 2
+
+# Concatenate Tuples:
+new_tuple = my_tuple + ("New", "Tuple")
+print("Concatenated tuple:", new_tuple)
